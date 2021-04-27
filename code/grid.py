62201cb5c589ed5d1c23cae950f6c024bd62ae30
legoffant <legoff.ant@gmail.com>
2021-04-27 21:20:50 +0200

    definition fonction grille

---
+++ code/grid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""Utilisation de l'exemple grid.py du livre pensez en python"""
+
+# mini grille 3x3
+print("+++")
+print("+ +")
+print("+++")
+
+#mini grille avec des fonctions
+
+def print_plus():
+    print("+", end=' ')
+
+def print_space():
+    print(" ", end=' ')
+
+
